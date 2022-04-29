f3952d25565f71c219022c8c50a88664bcea8a29
Eric5846 <104625169+Eric5846@users.noreply.github.com>
2022-04-29 12:56:44 +0200

    made nullable expiration

--- merch/listings/models.py
+++ merch/listings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Ref(models.Model):
 	gtin = models.fields.CharField(max_length=100)
-	expiration_date = models.fields.DateField()
+	expiration_date = models.fields.DateField(null=True)
 
 	def __str__(self):
 		return f'{self.gtin}'
